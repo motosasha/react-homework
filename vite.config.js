29aa470c3cb2a967ca813fbb7746639f1e73dd3a
motosasha <moto.sasha@gmail.com>
2026-09-12 00:19:50 +0300

    chores: clean project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@assets": path.resolve(__dirname, "./src/assets"),
+			"@components": path.resolve(__dirname, "./src/components"),
+			"@styles": path.resolve(__dirname, "./src/styles"),
+			"@utils": path.resolve(__dirname, "./src/utils")
+		}
+	}
+});
